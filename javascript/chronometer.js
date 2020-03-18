735e04f8e305ad749bbd37d0ba031b3a1af35a58
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-03-18 14:33:41 -0300

    add constructor, startClick and getMinutes methods

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,16 +1,21 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
+
   getSeconds() {
     // ... your code goes here
   }
+  
   twoDigitsNumber() {
     // ... your code goes here
   }
